65fabd0acf6ad6558451fc2ec3be6de52de4bb0b
Iury O. g. Figueiredo <tau@ocean.ocean.net>
2015-08-28 18:46:04 -0300

    Adding docs.

--- vyapp/plugins/select_sym_pair.py
+++ vyapp/plugins/select_sym_pair.py
@@ -1,10 +1,24 @@
 """
+Overview
+========
+This plugin implements a Key-Command to select text between pairs of () [] {}.
 
-Mode: 1
+Usage
+=====
+
+When the cursor is placed over one of the () [] {} and the key press event
+<Key-slash> happens in NORMAL mode then the text between the pair will be selected.
+
+Such a behavior is useful when dealing with some programming languages like lisp.
+
+
+Key-Commands
+============
+
+Mode: NORMAL
 Event: <Key-slash> 
 Description: Select text between pairs of ( ) [] {} when the cursor
 is placed over one of these characters.
-
 """
 
 
@@ -12,5 +26,6 @@ def install(area):
     area.install(('NORMAL', '<Key-slash>', lambda event: event.widget.select_case_pair(('(', ')'))),
                  ('NORMAL', '<Key-slash>', lambda event: event.widget.select_case_pair(('[', ']'))),
                  ('NORMAL', '<Key-slash>', lambda event: event.widget.select_case_pair(('{', '}'))))
+
 
 
--- vyapp/plugins/select_text.py
+++ vyapp/plugins/select_text.py
@@ -1,15 +1,34 @@
 """
-Mode: 1
+Overview
+========
+
+This plugin implements Key-Commands to select ranges of text to the beginning/end of the file from the
+cursor position.
+
+Usage
+=====
+
+A simple way to select all text from the cursor position to the end of the AreaVi instance is by
+pressing <Control-Key-2> in NORMAL mode. The same behavior can be achieved to select
+all text to the beginning of the AreaVi instance by pressing <Control-Key-1> in NORMAL mode as well.
+
+Another possibility is selecting all text of the AreaVi instance, for such just press 
+<Control-a> in NORMAL mode.
+
+Key-Commands
+============
+
+Mode: NORMAL
 Event: <Control-Key-1> 
 Description: Add selection from the cursor positon to the beginning of the file.
 
 
-Mode: 1
+Mode: NORMAL
 Event: <Control-Key-2> 
 Description: Add selection from the cursor position to the end of the file.
 
 
-Mode: 1
+Mode: NORMAL
 Event: <Control-a> 
 Description: Add selection from the beginning to the end of the file.
 """
@@ -19,4 +38,5 @@ def install(area):
     area.install(('NORMAL', '<Control-Key-1>', lambda event: event.widget.sel_text_start()),
                  ('NORMAL', '<Control-Key-2>', lambda event: event.widget.sel_text_end()),
                  ('NORMAL', '<Control-a>', lambda event: event.widget.select_all()))
+
 
