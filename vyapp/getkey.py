b113e07c2ac802b5e128c249d88ac55f2ccb3938
Iury O. g. Figueiredo <tau@ocean.ocean.net>
2015-09-09 18:56:06 -0300

    Factoring.

---
+++ vyapp/getkey.py
@@ -0,0 +1,9 @@
+class GetKey(object):
+    """
+    This class abstracts the process of getting data from key press events 
+    on an AreaVi instance.
+    """
+
+    def __init__(self, area):
+        pass
+
