390de8024ddb96521d24115c865c58073ae61b55
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2015-09-17 16:04:20 -0300

    Finishing doors plugin.

--- vyapp/call.py
+++ vyapp/call.py
@@ -3,43 +3,58 @@ from threading import Thread
 from Queue import Queue, Empty
 from os import environ, setsid, killpg
 
-TIMEOUT = 200
-    
 class Call(Thread):
     def __init__(self, *args):
-        Thread.__init__(self)
+        """
+        """
+
         self.child = Popen(args, shell=0, stdout=PIPE, stdin=PIPE,  
                                             stderr=STDOUT,  env=environ)
+        self.args  = args
         self.queue = Queue()
+        self.stop  = False
+        self.base  = []
+        Thread.__init__(self)
         self.start()
-        self.stop = False
-        self.base = []
 
     def send(self, data):
-        self.child.stdin.write(data.encode('utf-8'))
+        """
+        """
+
+        data = data.encode('utf-8')
+        self.child.stdin.write(data)
 
     def run(self):
+        """
+        """
+
         while not self.stop:
-            data = self.child.stdout.readline()
-            self.queue.put_nowait(data)
+            self.queue.put_nowait(self.child.stdout.readline())
+        self.child.wait()
 
     def update(self):
-        while True:
-            try:
-                data = self.queue.get_nowait()
-            except Empty:
-                break
-            else:
-                self.dispatch(data)
-                
+        """
+        """
+
+        while not self.queue.empty():
+            self.dispatch(self.queue.get_nowait())
+            
     def add_handle(self, handle):
+        """
+        """
+
         self.base.append(handle)
 
     def dispatch(self, data):
+        """
+        """
+
         for ind in self.base:
             ind(data)
 
     def die(self):
+        """
+        """
         self.stop = True
         self.child.kill()
 
@@ -48,6 +63,8 @@ def run_reactor(widget, reactor, timeout=200):
         widget.after(timeout, loop)               
         reactor.update()    
     widget.after(timeout, loop)               
+
+
 
 
 
--- vyapp/plugins/doors.py
+++ vyapp/plugins/doors.py
@@ -1,15 +1,37 @@
+"""
+Overview
+========
+
+Usage
+=====
+
+
+Key-Commands
+============
+
+"""
+
 from vyapp.call import Call, run_reactor
 from vyapp.ask import Ask
 from vyapp.tools import set_status_msg
+from Tkinter import TclError
 import shlex
 
 class Command(object):
     def __init__(self, area):
-        area.install(('NORMAL', '<Control-F4>', lambda event: self.start_process(event.widget)))
+        """
+
+        """
+
+        area.hook('NORMAL', '<Control-F4>', 
+                        lambda event: self.start_process(event.widget))
 
     def start_process(self, area):
-        ask = Ask(area)
+        """
 
+        """
+
+        ask = Ask(area)
         if not ask.data: 
             return
 
@@ -20,25 +42,60 @@ class Command(object):
         else:
             self.create_output_area(area, call)
 
+    def kill_process(self, call):
+        """
+
+        """
+
+        # Kill the process regardless whether it is the
+        # output area or not.
+        try:
+            call.die()
+        except OSError:
+            pass
+
+        # The <Destroy> is spawned after the statusbar has been
+        # destroyed.
+        try:
+            set_status_msg('Killed process!')
+        except TclError:
+            pass
+    
     def create_output_area(self, area, call):
+        """
+
+        """
+
         output = area.master.master.create()
-        output.hook(-1, '<Destroy>', lambda event: call.die())
+    
+        # When one of the AreaVi instances are destroyed then
+        # the process is killed.
+        output.hook(-1, '<Destroy>', lambda event: self.kill_process(call))
+        area.hook(-1, '<Destroy>', lambda event: self.kill_process(call))
 
         call.add_handle(lambda data: self.handle_read(output, data))
-        area.hook(-1, '<Destroy>', lambda event: call.die())
+
         area.hook('NORMAL', '<F9>', lambda event: self.dump_line(event.widget, call))
         run_reactor(area, call)
 
-
     def dump_line(self, area, call):
+        """
+
+        """
+
         line = area.get('insert linestart', 'insert +1l linestart')
         call.send(line)
 
     def handle_read(self, area, data):
+        """
+
+        """
+
         area.insert('end', data)
         area.mark_set('insert', 'end')
         area.see('end')
 
 install = Command
+
 
 
