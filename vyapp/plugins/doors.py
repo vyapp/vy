521c623f9203fc13841b3c8eeb70aeec6e871f6c
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2015-09-21 21:49:46 -0300

    Factoring.

--- vyapp/plugins/doors.py
+++ vyapp/plugins/doors.py
@@ -11,7 +11,8 @@ Key-Commands
 
 """
 
-from vyapp.call import Call, run_reactor
+from untwisted.expect import Expect, LOAD, CLOSE
+from untwisted.usual import xmap
 from vyapp.ask import Ask
 from vyapp.tools import set_status_msg
 from Tkinter import TclError
@@ -34,9 +35,8 @@ class Command(object):
         ask = Ask(area)
         if not ask.data: 
             return
-
         try:
-            call = Call(*shlex.split(ask.data))
+            call = Expect(*shlex.split(ask.data))
         except Exception as e:
             set_status_msg(e)
         else:
@@ -46,11 +46,10 @@ class Command(object):
         """
 
         """
-
         # Kill the process regardless whether it is the
         # output area or not.
         try:
-            call.die()
+            call.destroy()
         except OSError:
             pass
 
@@ -73,10 +72,14 @@ class Command(object):
         output.hook(-1, '<Destroy>', lambda event: self.kill_process(call))
         area.hook(-1, '<Destroy>', lambda event: self.kill_process(call))
 
-        call.add_handle(lambda data: self.handle_read(output, data))
+        xmap(call, LOAD, lambda expect, data: self.handle_read(output, data))
+        xmap(call, CLOSE, self.handle_close)
 
         area.hook('NORMAL', '<F9>', lambda event: self.dump_line(event.widget, call))
-        run_reactor(area, call)
+
+    def handle_close(self, expect):
+        print 'closed'
+
 
     def dump_line(self, area, call):
         """
@@ -90,12 +93,12 @@ class Command(object):
         """
 
         """
-
         area.insert('end', data)
         area.mark_set('insert', 'end')
         area.see('end')
 
 install = Command
+
 
 
 
