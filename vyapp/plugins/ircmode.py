beee02b3bfaabe7d0c39de3837f1aa9ad6f63f58
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2015-09-29 18:07:54 -0300

    Factoring out.

--- vyapp/plugins/ircmode.py
+++ vyapp/plugins/ircmode.py
@@ -44,7 +44,6 @@ def on_part(con, nick, user, host, chan):
 
 def on_001(con, address, nick, *args):
     con.nick = nick
-    print 'the nick is', nick
 
 class IrcMode(object):
     def __init__(self, area):
@@ -60,11 +59,10 @@ class IrcMode(object):
         port       = int(port)
         con.connect_ex((addr, port))
         Client(con)
+
         xmap(con, CONNECT, lambda con: self.set_up_con(con, area))
         xmap(con, CONNECT_ERR, self.on_connect_err)
-        area.hook('IRC', '<Control-e>', lambda event: self.send_cmd(event.widget, con))
-        area.mark_set('addr', '1.0')
-        xmap(con, FOUND, lambda con, data: area.insert('addr', '%s\n' % data))
+
 
     def send_cmd(self, area, con):
         ask = Ask(area)
@@ -75,27 +73,51 @@ class IrcMode(object):
         Stdout(con)
         Shrug(con)
         Irc(con)
-        xmap(con, '001', on_001)
-        xmap(con, 'PRIVMSG', on_privmsg)
-        xmap(con, 'JOIN', on_join)
-        xmap(con, 'PART', on_part)
-        xmap(con, '353', on_353)
-        xmap(con, 'MEJOIN', lambda con, chan: self.create_channel(area, con, chan))
-        xmap(con, 'PING', lambda con, prefix, servaddr: 
-                send_cmd(con, 'PONG :%s' % servaddr))
+
+        self.set_common_irc_handles(area, con)
+        self.set_common_irc_commands(area, con)
 
     def create_channel(self, area, con, chan):
         area_chan = root.note.create(chan)
         area_chan.chmode('IRC')
 
-        area_chan.hook('IRC', '<Control-e>', lambda event: self.send_cmd(event.widget, con))
-        area_chan.hook('IRC', '<Return>', 
-                lambda event: self.send_chan_msg(event.widget, chan, con))
+        self.set_common_chan_commands(area_chan, con, chan)
+        self.set_common_chan_handles(area_chan, con, chan)
+    
+    def set_common_irc_commands(self, area, con):
+        area.hook('IRC', '<Control-e>', lambda event: self.send_cmd(event.widget, con))
 
-        xmap(con, 'PRIVMSG->%s' % chan, lambda *args: self.on_privmsg(area_chan, *args))
-        xmap(con, '332', lambda *args: self.on_332(area_chan, *args))
+    def set_common_irc_handles(self, area, con):
+        l1 = lambda con, chan: self.create_channel(area, con, chan)
+        l2 = lambda con, prefix, servaddr: send_cmd(con, 'PONG :%s' % servaddr)
+        l3 = lambda con, data: area.insert('end', '%s\n' % data)
 
-        # xmap(con, CLOSE, lambda *args: )
+        xmap(con, '001', on_001)
+        xmap(con, 'PRIVMSG', on_privmsg)
+        xmap(con, 'JOIN', on_join)
+        xmap(con, 'PART', on_part)
+        xmap(con, '353', on_353)
+        xmap(con, 'MEJOIN', l1)
+        xmap(con, 'PING', l2)
+        xmap(con, FOUND, l3)
+
+    def set_common_chan_commands(self, area, con, chan):
+        e1 = lambda event: self.send_chan_msg(event.widget, chan, con)
+        e2 = lambda event: self.send_cmd(event.widget, con)
+
+        area.hook('IRC', '<Return>', e1)
+        area.hook('IRC', '<Control-e>', e2)
+
+    def set_common_chan_handles(self, area, con, chan):
+        l1 = lambda *args: self.on_privmsg(area, *args)
+        l2 = lambda *args: self.on_332(area, *args)
+
+        xmap(con, 'PRIVMSG->%s' % chan, l1)
+        xmap(con, '332', l2)
+
+
+    def on_part(self, area, *args):
+        pass
 
     def send_chan_msg(self, area, chan, con):
         data = area.get('insert linestart', 'insert lineend')
@@ -105,6 +127,8 @@ class IrcMode(object):
 
     def on_privmsg(self, area, con, nick, user, host, msg):
         area.insert('end', '<%s> %s\n' % (nick, msg))
+        area.mark_set('insert', 'end')
+        area.see('insert')
 
     def on_332(self, area, con, addr, nick, channel, msg):
         area.insert('end', 'Topic: %s\n' % msg)
@@ -119,4 +143,5 @@ def ircmode():
 
 ENV['ircmode'] = ircmode
 install        = IrcMode
+
 
