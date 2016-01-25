a31072be89459b6a7324cd50414d72b3bb226fb2
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-01-25 17:01:48 -0200

    Adding docs.

--- vyapp/notevi.py
+++ vyapp/notevi.py
@@ -1,5 +1,5 @@
 """
-
+This module implements widgets that permit managing tabs, panes in vy.
 """
 
 from Tkinter import *
@@ -8,19 +8,18 @@ from ttk import Notebook
 
 class PanedHorizontalWindow(PanedWindow):
     """
-
+    This widget is used to create horizontal panes.
     """
 
     def __init__(self, *args, **kwargs):
-        """
-
-        """
-
         PanedWindow.__init__(self, orient=HORIZONTAL, *args, **kwargs)
 
     def create_area(self, filename='none'):
         """
+        This method creates an AreaVi widget whose AreaVil.filename attribute is
+        the argument filename. It returns the AreaVi widget that was created.
 
+        The plugins that appear in the vyrc file will be installed in the AreaVi widget.
         """
 
         frame     = Frame(master=self)
@@ -47,7 +46,9 @@ class PanedHorizontalWindow(PanedWindow):
 
     def create(self, filename='none'):
         """
-
+        This method creates a horizontal AreaVi widget. It returns the
+        AreaVi widget that was created. It as well installs the plugins
+        that appear in the vyrc file in the AreaVi widget.
         """
 
         area = self.create_area(filename)
@@ -56,7 +57,9 @@ class PanedHorizontalWindow(PanedWindow):
 
     def load(self, filename):
         """
-
+        It creates a horizontal split and loads the content of filename
+        into the new AreaVi instance. It returns the AreaVi widget that
+        was created.
         """
 
         area = self.create_area()
@@ -66,19 +69,17 @@ class PanedHorizontalWindow(PanedWindow):
 
 class PanedVerticalWindow(PanedWindow):
     """
-
+    This widget implements vertical panes.
     """
 
     def __init__(self, *args, **kwargs):
-        """
-
-        """
-
         PanedWindow.__init__(self, orient=VERTICAL, *args, **kwargs)
 
     def create(self, filename='none'):
         """
-
+        This method creates a new horizontal window in which
+        it is possible to create new horizontal splits. The argument filename
+        is set as attribute for the new AreaVi widget.
         """
 
         base = PanedHorizontalWindow(master=self)
@@ -88,7 +89,9 @@ class PanedVerticalWindow(PanedWindow):
 
     def load(self, *args):
         """
-
+        This method adds a new horizontal window and loads
+        the content of the files passed as args in the new AreaVi
+        widgets.
         """
 
         base = PanedHorizontalWindow(master=self)
@@ -100,19 +103,16 @@ class PanedVerticalWindow(PanedWindow):
 
 class NoteVi(Notebook):
     """
-
+    This class implements vy tabs.
     """
 
     def __init__(self, *args, **kwargs):
-        """
-
-        """
-
         Notebook.__init__(self, *args, **kwargs)
 
     def create(self, filename):
         """
-
+        This method creates a new tab whose title is the string
+        passed as filename.
         """
 
         base = PanedVerticalWindow(master=self)
@@ -122,7 +122,13 @@ class NoteVi(Notebook):
 
     def load(self, *args):
         """
+        This method opens the files that are specified in args into new tabs and panes.
+        The structure of args is like:
 
+        args = ((('file1', 'file2'), ('file3', 'file4')), (('file5', 'file6'), ))
+
+        It would create two tabs, the first tab would have four panes, the second tab
+        would be two panes.
         """
 
         for indi in args:
@@ -131,9 +137,6 @@ class NoteVi(Notebook):
             self.add(base)        
             for indj in indi:
                 base.load(*indj)
-
-
-
 
 
 
