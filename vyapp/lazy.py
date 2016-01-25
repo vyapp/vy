8706a7244859b70ec402c0fc19e1e030c652d15c
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-01-25 14:58:25 -0200

    Adding docs.

--- vyapp/lazy.py
+++ vyapp/lazy.py
@@ -1,4 +1,19 @@
+"""
+This module implements tools to execute python code asynchronously with tkinter mainloop.
+"""
+
 def consume_iter(iterator, time=1):
+    """
+    This function receives an iterator that is consumed from tkinter update
+    function. It is a way to have python code executed asynchronously.  Some
+    plugins would perform heavy operations that could block tkinter mainloop,
+    these plugins should write code that can be executed asynchronously using  
+    iterators.
+
+    Note: Some plugins like syntax highlighting would use this technique
+    to highlight code. 
+    """
+
     def cave():
         from vyapp.app import root
         try:
@@ -9,4 +24,5 @@ def consume_iter(iterator, time=1):
             root.after(time, cave)
 
     cave()
+
 
