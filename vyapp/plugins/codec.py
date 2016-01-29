b520d15b11fc7a4b708d378764dbe5c428261de7
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-01-29 16:38:35 -0200

    Adding docs.

--- vyapp/plugins/codec.py
+++ vyapp/plugins/codec.py
@@ -1,5 +1,32 @@
 """
+Overview
+========
 
+This module implements functions to adjust AreaVi widget encoding.
+
+Usage
+=====
+
+The function below defines the encoding to save files:
+
+    charset(name)
+
+The function:
+
+    decode(name)
+
+is used to adjust the encoding of the AreaVi widget text.
+
+Commands
+========
+
+Command: charset(name)
+Description: Set the default encoding to save files.
+name = The name of the encoding.
+
+Command: decode(name)
+Description: Adjust an AreaVi widget text encoding.
+name = The name of the encoding.
 """
 
 from vyapp.plugins import ENV
@@ -18,4 +45,5 @@ def charset(name):
 
 ENV['decode']  = decode
 ENV['charset'] = charset
+
 
--- vyapp/plugins/url_source.py
+++ vyapp/plugins/url_source.py
@@ -1,3 +1,27 @@
+"""
+Overview
+========
+
+This module implements a keycommand to download the source code of the url that is
+in the clipboard.
+
+Usage
+=====
+
+In order to have the url source code inserted in an AreaVi instance it is neeeded to set an output
+target. Copy a URL address to the clipboard then switch to BETA mode and press <Key-m>.
+The source code that corresponds to the URL address will be inserted in the
+AreaVi instance that was set as output target.
+
+Key-Commands
+============
+
+Mode: BETA
+Event: <Key-m>
+Description: Download the source code of the URL that is in the clipboard
+and writes it to sys.stdout.
+"""
+
 import sys
 import urllib2
 
@@ -9,6 +33,7 @@ def install(area):
         sys.stdout.write(req.read())
 
     area.hook('BETA', '<Key-m>', lambda event: get_url_source())
+
 
 
 
