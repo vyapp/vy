95428ddb264874b01137339135ff17ca2d159664
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-03-03 15:04:51 -0400

    Improving spider plugin.

--- vyapp/plugins/syntax/spider.py
+++ vyapp/plugins/syntax/spider.py
@@ -3,41 +3,8 @@
 
 from pygments.lexers import get_lexer_for_filename 
 from vyapp.plugins.syntax.tools import *
+from vyapp.plugins.syntax.keys import PRECEDENCE_TABLE, DEFAULT
 from vyapp.exe import exec_quiet
-
-TAG_KEYS_PRECEDENCE =('Token.Name.Tag',
-    'Token.Comment.Multiline',
-    'Token.Literal.String.Doc',
-    'Token.Literal.String.Heredoc',
-    'Token.Literal.String.Regex',
-    'Token.Literal.String.Symbol',
-    'Token.Literal.String',
-    'Token.Literal.String.Other',
-    'Token.Literal.String.Single',
-    'Token.Comment.Preproc',
-    'Token.Comment',
-    'Token.Comment.Single',
-    'Token.Comment.Special',
-    'Token.Literal.String.Double',
-    'Token.Literal.String.Backtick',
-    'Token.Literal.String.Char',
-    'Token.Literal.Number.Float',
-    'Token.Punctuation',
-    'Token.Operator',
-    'Token.Name',
-    'Token.Text',
-    'Token.Keyword',
-    'Token.Generic',
-    'Token.Generic.Deleted',
-    'Token.Generic.Emph',
-    'Token.Generic.Error',
-    'Token.Generic.Heading',
-    'Token.Generic.Inserted',
-    'Token.Generic.Output',
-    'Token.Generic.Prompt',
-    'Token.Generic.Strong',
-    'Token.Generic.Subheading',
-    'Token.Generic.Traceback')
 
 def install(area, theme, max=10):
     """
@@ -49,6 +16,7 @@ def install(area, theme, max=10):
         except Exception:
             return
 
+        TAG_KEYS_PRECEDENCE = PRECEDENCE_TABLE.get(tuple(lexer.aliases), DEFAULT)
         index0 = area.index('@0,0')
         index0 = area.index('%s -%sl' % (index0, max))
         index0 = area.tag_next_occur(TAG_KEYS_PRECEDENCE, index0, 'insert', '1.0')
@@ -73,6 +41,7 @@ def install(area, theme, max=10):
                  (-1, '<Escape>', lambda event: update_scheme()))
 
     setup_theme_scheme(area, theme)
+
 
 
 
