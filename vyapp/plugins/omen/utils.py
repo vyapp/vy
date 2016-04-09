44b9a125716e21565dc2b3d42979176b49e19729
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-04-09 12:15:49 -0400

    Improving auto completion scheme.

--- vyapp/plugins/omen/utils.py
+++ vyapp/plugins/omen/utils.py
@@ -16,8 +16,9 @@ class PythonCompleteWindow(CompleteWindow):
         completions = script.completions()
 
         CompleteWindow.__init__(self, area, completions, *args, **kwargs)
-        self.bind('<F1>', lambda event: sys.stdout.write('%s\n' % self.box.elem_desc()))
+        self.bind('<F1>', lambda event: sys.stdout.write('%s\n%s\n' % ('#' * 80, self.box.elem_desc())))
 
         self.area.wait_window(self)
+
 
 
