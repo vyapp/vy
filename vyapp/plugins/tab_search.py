a9f3623f129de1c2325a463914a1d227e9b43f1f
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-04-12 12:12:14 -0400

    Improving tab search.

--- vyapp/plugins/tab_search.py
+++ vyapp/plugins/tab_search.py
@@ -3,20 +3,19 @@
 """
 from vyapp.ask import Ask
 from vyapp.app import root
-from Tkinter import DISABLED
 
 class TabSearch(object):
     def __init__(self, area):
         self.area = area
-        area.install(('NORMAL', '<Key-space>', self.start_search))
+        area.install((-1, '<Control-space>', self.start_search))
 
 
     def start_search(self, event):
         ask = Ask(self.area, wait=False)
         self.ask = ask
         ask.bindtags(('Entry', ask, '.', 'all'))
-        ask.bind('<Control-h>', self.next_tab)
-        ask.bind('<Control-l>', self.prev_tab)
+        ask.bind('<Control-k>', self.prev_tab)
+        ask.bind('<Control-j>', self.next_tab)
         ask.bind('<Key>', self.update_search)
         ask.bind('<Destroy>', lambda event: root.note.set_area_focus())
 
@@ -38,8 +37,9 @@ class TabSearch(object):
         """
 
         """
-        if self.index < len(self.index): self.index = self.index + 1
-
+        if self.index < len(self.seq) - 1: self.index = self.index + 1
+        else: return
+        root.after(30, lambda : self.ask.focus_set())
         root.note.select(self.seq[self.index])
 
     def prev_tab(self, event):
@@ -47,10 +47,11 @@ class TabSearch(object):
 
         """
         if self.index > 0: self.index = self.index - 1
+        else: return
+        root.after(30, lambda : self.ask.focus_set())
         root.note.select(self.seq[self.index])
 
     
 install = TabSearch
-
 
 
