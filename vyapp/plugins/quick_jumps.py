74f26c1d95e330c0de0c9640e2e095d0a1986319
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-05-03 17:51:29 -0400

    Improving quick_jumps scheme, removing unecessary stuff.

--- vyapp/plugins/quick_jumps.py
+++ vyapp/plugins/quick_jumps.py
@@ -1,10 +1,34 @@
+"""
+Overview
+========
+
+It is used to remember an important position of the text and quickly switch to it.
+
+
+Usage
+=====
+
+When <Key-space> is pressed in NORMAL mode then a mark is dropped at the cursor position. It is possible to set the cursor back to that
+mark by pressing <BackSpace> in NORMAL mode.
+
+Key-Commands
+============
+
+Mode: NORMAL
+Event: 
+Description: 
+
+Mode: NORMAL
+Event: 
+Description: 
+
+"""
 
 class QuickJumps(object):
     def __init__(self, area):
         self.area  = area
-        area.install(('NORMAL', '<Key-m>', lambda event: self.area.mark_set('(PREV_POSITION)', 'insert')),
-                     ('NORMAL', '<Key-n>', lambda event: self.area.mark_set('(PREV_POSITION)', 'insert')),
-                     ('NORMAL', '<Key-i>', lambda event: self.area.mark_set('(PREV_POSITION)', 'insert')),
+        area.install(('NORMAL', '<Key-space>', lambda event: self.area.mark_set('(PREV_POSITION)', 'insert')),
                      ('NORMAL', '<BackSpace>', lambda event: self.area.seecur('(PREV_POSITION)')))
 
 install = QuickJumps
+
