0d1bc637a05c1ce13c105b20cadc85db9f9fc92b
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-05-05 18:19:07 -0400

    Adding keycommand to ALPHA mode to quickly open in the browser the current opened file.

--- vyapp/plugins/quick_url.py
+++ vyapp/plugins/quick_url.py
@@ -16,14 +16,25 @@ Key-Commands
 Mode: ALPHA
 Event: <Key-l>
 Description: Open the url over the cursor in the browser, a new tab is opened.
+
+Mode: ALPHA
+Event: <Key-L>
+Description: Load the focused AreaVi instance file in the browser.
 """
 
 import webbrowser
 
 def open_url(event):
     webbrowser.open_new_tab(event.widget.get_seq('insert'))
+    event.widget.chmode('NORMAL')
+
+def open_file(event):
+    webbrowser.open_new_tab(event.widget.filename)
+    event.widget.chmode('NORMAL')
 
 def install(area):
-    area.install(('ALPHA', '<Key-l>', open_url))
+    area.install(('ALPHA', '<Key-l>', open_url),
+                 ('ALPHA', '<Key-L>', open_file))
+
 
 
