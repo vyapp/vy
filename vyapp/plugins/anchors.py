13d54d60391a533e68f2335ca7af68cdde07629a
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-07-18 19:53:56 -0400

    adding anchors plugin.

---
+++ vyapp/plugins/anchors.py
@@ -0,0 +1,22 @@
+class Anchors(object):
+    def __init__(self, area):
+        area.add_mode('ANCHORS')
+        self.area  = area
+        area.install(('NORMAL', '<Alt-b>', lambda event: area.chmode('ANCHORS')),
+                     ('ANCHORS', '<Key>', self.drop_anchor))
+
+    def drop_anchor(self, event):
+        try:
+            name = '(ANCHORS-%s)' % event.keysym
+            pos0 = self.area.index(name)
+        except Exception:
+            self.area.mark_set(name, 'insert')
+        else:
+            if not self.area.compare(pos0, '==', 'insert'):
+                self.area.mark_set('insert', name)
+            else:
+                self.area.mark_unset(name)
+        self.area.chmode('NORMAL')
+
+install = Anchors
+
