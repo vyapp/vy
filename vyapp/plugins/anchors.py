90f86c5c6363e2f218b9d535ec2c6b406a9f9e4b
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-07-29 12:42:58 -0400

    Fixing bad behavior with anchors plugin.

--- vyapp/plugins/anchors.py
+++ vyapp/plugins/anchors.py
@@ -1,24 +1,24 @@
+"""
+
+"""
+
 class Anchors(object):
     def __init__(self, area):
         area.add_mode('ANCHORS')
         self.area  = area
         area.install(('NORMAL', '<Alt-b>', lambda event: area.chmode('ANCHORS')),
-                     ('ANCHORS', '<Key>', self.drop_anchor))
+                     ('ANCHORS', '<Control-Key>', self.drop),
+                     ('ANCHORS', '<Key>', self.jump))
 
-    def drop_anchor(self, event):
-        try:
-            name = '(ANCHORS-%s)' % event.keysym
-            pos0 = self.area.index(name)
-        except Exception:
-            self.area.mark_set(name, 'insert')
-        else:
-            if self.area.compare(pos0, '==', 'insert'):
-                self.area.mark_unset(name)
-            else:
-                self.area.seecur(name)
+    def drop(self, event):
+        self.area.mark_set('(ANCHORS-%s)' % event.keysym, 'insert')
+        self.area.chmode('NORMAL')
 
+    def jump(self, event):
+        self.area.seecur('(ANCHORS-%s)' % event.keysym)
         self.area.chmode('NORMAL')
 
 install = Anchors
+
 
 
