cfd4eed537ea014a0fa61c8e83b7a1b8b0f0b6fe
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-08-09 01:53:15 -0400

    Improving shell plugin.

--- vyapp/plugins/shell.py
+++ vyapp/plugins/shell.py
@@ -7,19 +7,8 @@ Used to spawn processes and send/receive data. It is useful to talk with extern 
 Usage
 =====
 
-Press <Control-F4> in NORMAL mode to open a vertical panel that will be used as output area.
-It will show an entry text to type the name of the command/program then press enter after specifying
-the sequence to run the process.
-
-Once the process is running then use <F2> in NORMAL mode or INSERT mode to dump the cursor line
-to the process. The process output will appear in the new AreaVi instance that was created.
-
 Key-Commands
 ============
-
-Mode: NORMAL
-Event: <Control-F4>
-Description: Instantiates an extern process.
 
 Mode: NORMAL
 Event: <F2>
@@ -36,87 +25,76 @@ from vyapp.ask import Ask
 from vyapp.tools import set_status_msg
 from vyapp.exe import exec_quiet
 from Tkinter import TclError
+from vyapp.plugins import ENV
+from vyapp.areavi import AreaVi
+
 import shlex
 
-class Pool(object):
-    def __init__(self):
-        self.base = []
-
-    def create(self, *args, **kwargs):
-        call = Expect(*args, **kwargs)
-        self.base.append(call)
-        return call
-
-    def send(self, data):
-        for ind in self.base:
-            ind.send(data)
-
-    def remove(self, call):
-        self.base.remove(call)
-
-class Command(object):
-    def __init__(self, area):
-        """
-
-        """
-
-        area.hook('NORMAL', '<Control-F4>', 
-                        lambda event: self.start_process(event.widget))
-        area.hook('NORMAL', '<F2>', lambda event: self.dump_line_and_down(event.widget))
-        area.hook('INSERT', '<F2>', lambda event: self.dump_line_and_insert_line(event.widget))
-        self.pool = Pool()
-
-    def start_process(self, area):
-        """
-
-        """
-
-        ask = Ask(area)
-        if not ask.data: 
-            return
+class Shell(object):
+    def __init__(self, data):
+        self.area = AreaVi.ACTIVE
 
         try:
-            call = self.pool.create(*shlex.split(ask.data))
+            self.expect = Expect(*shlex.split(data))
         except Exception as e:
             set_status_msg(e)
         else:
-            self.create_output_area(area, call)
-
-    def create_output_area(self, area, call):
+            self.create_output()
+        
+    def create_output(self):
         """
 
         """
 
-        output = area.master.master.create()
+        self.output = self.area.master.master.create()
     
         # When one of the AreaVi instances are destroyed then
         # the process is killed.
-        output.hook(-1, '<Destroy>', lambda event: exec_quiet(call.terminate))
-        area.hook(-1, '<Destroy>', lambda event: exec_quiet(call.terminate))
+        self.output.hook(-1, '<Destroy>', lambda event: 
+        self.terminate_process())
+        self.area.hook(-1, '<Destroy>', lambda event: 
+        self.terminate_process())
 
+        self.output.hook('NORMAL', '<Control-F2>', lambda event: 
+        self.map_commands())
+
+        self.map_commands()
         # When call.terminnate is called it may happen of having still data to be
         # processed. It would attempt to write on an AreaVi instance that no more exist.
         # So, it executes quietly the AreaVi.append method.
-        xmap(call, LOAD, lambda expect, data: exec_quiet(output.append, data))
-        xmap(call, CLOSE, self.handle_close)
+        xmap(self.expect, LOAD, lambda expect, data: 
+        exec_quiet(self.output.append, data))
 
-    def dump_line_and_down(self, area):
-        self.pool.send(area.curline())
-        area.down()
+        xmap(self.expect, CLOSE, self.handle_close)
 
-    def dump_line_and_insert_line(self, area):
-        self.pool.send(area.curline())
-        area.down()
+    def map_commands(self):
+        self.area.hook('NORMAL', '<F2>', lambda event: 
+        self.dump_line_and_down(), add=False)
+
+        self.area.hook('INSERT', '<F2>', lambda event: 
+        self.dump_line_and_insert_line(), add=False)
+        set_status_msg('%s -> %s' % (self.area.filename, self.output.filename))
+
+    def terminate_process(self):
+        try:
+            self.expect.terminate()
+        except Exception:
+            pass
+        set_status_msg('Killed process!')
+
+    def dump_line_and_down(self):
+        self.expect.send(self.area.curline())
+        self.area.down()
+
+    def dump_line_and_insert_line(self):
+        self.expect.send(self.area.curline())
+        self.area.down()
 
     def handle_close(self, expect):
         set_status_msg('Killed process!')
-        self.pool.remove(expect)
         expect.destroy()
 
-install = Command
-
-
-
+ENV['Shell']  = Shell
 
 
 
