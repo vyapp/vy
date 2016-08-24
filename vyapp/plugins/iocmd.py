64d7d8fda966ed7e6455d6061bd7d07b7048e7dc
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-08-24 17:15:27 -0400

    Adding quit command.

--- vyapp/plugins/iocmd.py
+++ vyapp/plugins/iocmd.py
@@ -27,6 +27,7 @@ from vyapp.areavi import AreaVi
 from vyapp.app import root
 
 ENV['s'] = lambda : AreaVi.ACTIVE.save_data()
+ENV['q'] = lambda : root.quit()
 ENV['ss'] = lambda filename: AreaVi.ACTIVE.save_data_as(filename)
 ENV['lo'] = lambda filename: AreaVi.ACTIVE.load_data(filename)
 ENV['to'] = lambda filename: root.note.load([ [filename] ])
@@ -35,6 +36,7 @@ ENV['hsplit'] = lambda : AreaVi.ACTIVE.master.master.create()
 
 
         
+
 
 
 
