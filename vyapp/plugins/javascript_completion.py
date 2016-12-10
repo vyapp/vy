a2ffeca6ca9efd7cffacb5d1426c26774bf2465b
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-12-10 00:18:02 -0200

    Improving javascript_completion plugin.

--- vyapp/plugins/javascript_completion.py
+++ vyapp/plugins/javascript_completion.py
@@ -15,7 +15,7 @@ completion.
 """
 
 from vyapp.completion import CompletionWindow, Option
-from subprocess import Popen
+from subprocess import Popen, PIPE
 import json
 import requests
 import sys
@@ -29,25 +29,37 @@ from vyapp.areavi import AreaVi
 filename = join(expanduser('~'), '.tern-config')
 if not exists(filename): copyfile(join(dirname(__file__), 'tern-config'), filename)
 
-class Tern(object):
-    def __init__(self, path):
-        self.path  = path
-        self.child = Popen([path, '--persistent'])
+class JavascriptCompletionWindow(CompletionWindow):
+    """
+    """
+
+    def __init__(self, path, port, area, *args, **kwargs):
+        # If ~/.tern-port doesn't exist then run the server.
+        self.path   = path
+        self.port   = port
+        source      = area.get('1.0', 'end')
+        line, col   = area.indcur()
+
+        if not exists(join(expanduser('~'), '.tern-port')): 
+            self.run_server()
+
+        completions = self.completions(source, line - 1, col, area.filename)
+
+        CompletionWindow.__init__(self, area, completions, *args, **kwargs)
+        self.bind('<F1>', lambda event: 
+        sys.stdout.write('/*%s*/\n%s\n' % ('*' * 80, 
+        self.box.selection_docs())))
+
+    def run_server(self):
+        self.child = Popen([self.path, '--port', str(self.port), 
+        '--persistent'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        self.child.stdout.readline()
         atexit.register(self.child.terminate)
 
-    def parse_port(self):
-        with open(join(getcwd(), '.tern-port'), 'r') as fd:
-            return int(fd.read())
-
-    def port(self):
-        port = self.parse_port()
-
-        def shell():
-            return port
-        self.port = shell
-        return port
-
     def completions(self, data, line, col, filename):
+        """
+        """
+
         payload = {
                     'query': { 
                                 'type': 'completions', 
@@ -62,7 +74,7 @@ class Tern(object):
                                "text": data}] 
                   }
         
-        addr = 'http://localhost:%s' % self.port()
+        addr = 'http://localhost:%s' % self.port
         req  = requests.post(addr, data=json.dumps(payload))
         return self.build(req.text)
 
@@ -70,23 +82,12 @@ class Tern(object):
         data = json.loads(data)
         return map(lambda ind: Option(**ind), data['completions'])
 
-class JavascriptCompletionWindow(CompletionWindow):
-    """
-    """
-
-    def __init__(self, tern, area, *args, **kwargs):
-        source      = area.get('1.0', 'end')
-        line, col   = area.indcur()
-        completions = tern.completions(source, line - 1, col, area.filename)
-
-        CompletionWindow.__init__(self, area, completions, *args, **kwargs)
-        self.bind('<F1>', lambda event: sys.stdout.write('/*%s*/\n%s\n' % ('*' * 80, self.box.selection_docs())))
-
 class JavascriptCompletion(object):
-    def __init__(self, area, tern):
-        self.tern = tern
+    def __init__(self, area, path='tern', port=1234):
+        self.path = path
+        self.port = port
         trigger = lambda event: area.hook('INSERT', '<Control-Key-period>', 
-                  lambda event: JavascriptCompletionWindow(self.tern, event.widget), add=False)
+                  lambda event: JavascriptCompletionWindow(self.path, self.port, event.widget), add=False)
         remove_trigger = lambda event: area.unhook('INSERT', '<Control-Key-period>')
         area.install((-1, '<<Load-application/x-javascript>>', trigger),
                      (-1, '<<Load-text/html>>', trigger), 
@@ -94,12 +95,11 @@ class JavascriptCompletion(object):
                      (-1, '<<Save-text/html>>', trigger), 
                      (-1, '<<LoadData>>', remove_trigger), (-1, '<<SaveData>>', remove_trigger))
 
-def javascript_tools(tern):
+def javascript_tools(path='tern', port=1234):
     active_completion = lambda :AreaVi.ACTIVE.hook('INSERT', '<Control-Key-period>', 
-                  lambda event: JavascriptCompletionWindow(tern, event.widget), add=False)
+                  lambda event: JavascriptCompletionWindow(path, port, event.widget), add=False)
     ENV['active_javascript_completion'] = active_completion
 
 install = JavascriptCompletion
-
 
 
