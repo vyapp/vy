2176ba93015bc663f494ee0062b12bddd63e6b13
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-03-05 20:46:50 -0300

    Organizing vyrc file, improving html_checker plugin.

--- vyapp/plugins/html_checker.py
+++ vyapp/plugins/html_checker.py
@@ -4,12 +4,28 @@ Overview
 
 Highlight html lines where errors/warnings were encountered by Html Tidy.
 
-Plugin dependencies: 
+When the plugin is installed in your vyrc, whenever an event
+of the type <<Save-text/html>> happens it runs tidy against the file
+and tags all regions where tidy found errors. 
 
-vyapp.plugins.text_spots
+It is possible to jump to these regions by using the keycommands implemented by
+the core text_spots plugin. 
+
+One would just jump back/next by pressing <Control-n> or <Control-m>
+in NORMAL mode. 
+
+The html checker plugin writes to sys.stdout all the errours
+that were encountered in the html file. it is necessary
+to set an output target on areavi instance in order to check
+the errors/warnings.
+
+For more information see: vyapp.plugins.text_spots
+
+Plugin dependencies: 
+    vyapp.plugins.text_spots
 
 Extern dependencies:
-Html Tidy
+    Html Tidy
 
 """
 
