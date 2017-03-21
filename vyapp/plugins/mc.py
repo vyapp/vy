8d61a2fa30eded7161ba668b3b404e793ebdc47a
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-03-21 00:24:28 -0300

    Improving mc plugin.

--- vyapp/plugins/mc.py
+++ vyapp/plugins/mc.py
@@ -1,4 +1,4 @@
-from subprocess import check_output
+from subprocess import check_output, call
 from os.path import expanduser, dirname, join
 from vyapp.app import root
 
@@ -11,11 +11,29 @@ class Mc(object):
 
         area.install(('NORMAL', '<Key-H>', lambda e: self.up()),
         ('NORMAL', '<Key-L>', lambda e: self.down()),
-        ('NORMAL', '<Key-Y>', self.cp),
-        ('NORMAL', '<Key-R>', self.cut),
-        ('NORMAL', '<Key-T>', self.paste),
-        ('NORMAL', '<Key-G>', self.open),
+        ('NORMAL', '<Key-Y>', lambda e: self.cp()),
+        ('NORMAL', '<Key-T>', lambda e: self.mv()),
+        ('NORMAL', '<Key-R>', lambda e: self.rm()),
+        ('NORMAL', '<Key-K>', lambda e: self.select()),
+        ('NORMAL', '<Control-g>', lambda e: self.clear_clipboard()),
+        ('NORMAL', '<Key-G>', lambda e: self.list_clipboard()),
+
+        ('NORMAL', '<Key-I>', self.open),
         ('NORMAL', '<Key-J>', lambda e:self.ls()))
+
+    def list_clipboard(self):
+        self.area.delete('1.0', 'end')
+        self.area.insert('1.0', 'Files in the clipboard!\n\n')
+        self.area.insert('end', '\n'.join(Mc.clipboard))
+
+    def clear_clipboard(self):
+        del Mc.clipboard[:]
+        root.status.set_msg('Cleared mc clipboard!')
+
+    def select(self):
+        filename = self.area.get_seq()
+        Mc.clipboard.append(filename)
+        root.status.set_msg('Appended %s!' % filename)
 
     def down(self):
         self.ph = self.area.get_seq()
@@ -26,22 +44,29 @@ class Mc(object):
         self.ls()
 
     def ls(self):
-        ph = join(self.ph, '*')
-        data = check_output('ls -d -1 %s' % ph, shell=1)
+        data = check_output('find %s -maxdepth 1' % self.ph, shell=1)
         self.area.delete('1.0', 'end')
         self.area.insert('1.0', data)
 
     def cp(self):
-        pass
+        destin = self.area.get_seq()
+        code = call('cp %s %s' % (' '.join(Mc.clipboard), destin), shell=1)
+        root.status.set_msg('Files copied!')
+        del Mc.clipboard[:]
+        self.ls()
 
-    def cut(self):
-        pass
+    def mv(self):
+        destin = self.area.get_seq()
+        code = call('mv %s %s' % (' '.join(Mc.clipboard), destin), shell=1)
+        root.status.set_msg('Files moved!')
+        del Mc.clipboard[:]
+        self.ls()
 
     def rm(self):
-        pass
-
-    def paste(self):
-        pass
+        code = call('rm %s' % ' '.join(Mc.clipboard), shell=1)
+        del Mc.clipboard[:]
+        root.status.set_msg('Deleted files!')
+        self.ls()
 
     def open(self, event):
         root.note.load([[self.area.get_seq()]])
