b711fa18082a73f3a8f87221fd60215745b8c0d0
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-05-27 16:35:36 -0300

    Adding docs, improving editrc.py plugin.

--- vyapp/plugins/editrc.py
+++ vyapp/plugins/editrc.py
@@ -1,10 +1,28 @@
 """
+Overview
+========
+
+Quickly edit your vyrc file.
+
+Key-Commands
+============
+
+Mode: ALPHA
+Event: <Alt-v>
+Description: Load your ~/.vy/vyrc file in the current
+AreaVi instance.
 
 """
+
 from vyapp.app import root
 
+def loadrc(event):
+    event.widget.load_data(root.rc)
+    event.widget.chmode('NORMAL')
+
 def install(area):
-    area.install('editrc', ('ALPHA', '<Key-v>', lambda event: event.widget.load_data(root.rc)))
+    area.install('editrc', ('ALPHA', '<Key-v>', loadrc))
+
 
 
 
--- vyapp/plugins/mode_shortcut.py
+++ vyapp/plugins/mode_shortcut.py
@@ -1,12 +1,26 @@
 """
+Overview
+========
+
+Implement keycommands to pin a given mode and easily switch back to it.
+
 
 Key-Commands
 ============
 
 Namespace: mode-shortcut
 
+Mode: GLOBAL
+Event: <Alt-n>
+Description: Pin the current mode.
+
+Mode: GLOBAL
+Event: <Alt-Escape>
+Description: Switch to the previous pinned mode.
+
 """
 
+from vyapp.app import root
 
 class ModeShortcut(object):
     target_id  = 'NORMAL'
@@ -14,15 +28,18 @@ class ModeShortcut(object):
 
     def __init__(self, area):
         self.area = area
-        area.install('mode-shortcut', ('-1', '<Alt-Escape>',  lambda event: self.area.chmode(self.target_id)),
+        area.install('mode-shortcut', ('-1', '<Alt-Escape>',  
+        lambda event: self.area.chmode(self.target_id)),
         ('-1', '<Alt-n>',  lambda event: self.save_mode()))
                      
     def save_mode(self):
         self.target_id = self.area.id
         self.area.chmode(self.DEFAULT_ID)
+        root.status.set_msg('Mode %s pinned!' % self.target_id)
         return 'break'
 
 install = ModeShortcut
+
 
 
 
