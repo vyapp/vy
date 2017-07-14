74c7f41e98af36e6f0635326cb7236b4e4e0b72c
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-07-14 04:38:10 -0300

    Improving scheme for opening files from command line.

--- vyapp/app.py
+++ vyapp/app.py
@@ -4,42 +4,56 @@ to the App class instance. The App class instance holds all vy editor's widgets.
 """
 
 from base import App, Debug
+import argparse
+import itertools
 import sys
 
-if __name__ != '__main__':
-    from optparse import OptionParser
+from optparse import OptionParser
 
-    parser = OptionParser()
-    parser.add_option("-l", "--lst", dest="lst",
-    help='''This option takes a string of the form:
-    "[[['file0', 'file1']], [['file2', 'file3']], ...]"
-    It displays files in different tabs and paned windows.''', 
-    metavar="string", default=[])
+parser = argparse.ArgumentParser()
 
-    parser.add_option("-v", "--debug", action="store_true", 
-    default=False, dest="debug")
-    (opt, args) = parser.parse_args()
+parser.add_argument('files', nargs='*', help='Files')
 
-    # It points to the root toplevel window of vy. 
-    # It is the one whose AreaVi instances
-    # are placed on. 
-    root = App()
-    lst  = eval(str(opt.lst))
-    lst  = lst + map(lambda ind: [[ind]], args)
-    
-    # It has to be called from here.
-    # otherwise the plugins will not
-    # be able to import vyapp.app.root
-    # variable.
-    root.create_vyrc()
+parser.add_argument(
+'-t', '--tab', dest='scheme', action='append_const', const=None,
+default=[], help='Instantiate a new tab. Ex: -t -p file0 file1')
 
-    if not lst: root.note.create('none')
-    else: root.note.load(*lst)
+parser.add_argument(
+'-p', '--panel', action='append', dest='scheme', default=[], nargs='+', 
+help='Open files in vertical/horizontal ways -p file0 file1 -p file2')
 
-    # It first waits vyrc file to be loaded in order to set sys.stderr.
-    # Otherwise errors when loading the vyrc file will be missed as well as when
-    # attempting to load a non existing file.
-    if not opt.debug: sys.stderr = Debug()
+parser.add_argument('-v', '--verbose', action='store_true',
+help='Show exceptions and messages.')
+
+args = parser.parse_args()
+
+lst = [list(g) for k, g in itertools.groupby(args.scheme, 
+lambda x: not x) if not k]
+
+# parser.add_option("-v", "--debug", action="store_true", 
+# default=False, dest="debug")
+# (opt, args) = parser.parse_args()
+
+# It points to the root toplevel window of vy. 
+# It is the one whose AreaVi instances
+# are placed on. 
+root = App()
+lst  = lst + map(lambda ind: [[ind]], args.files)
+
+# It has to be called from here.
+# otherwise the plugins will not
+# be able to import vyapp.app.root
+# variable.
+root.create_vyrc()
+
+if not lst: root.note.create('none')
+else: root.note.load(*lst)
+
+# It first waits vyrc file to be loaded in order to set sys.stderr.
+# Otherwise errors when loading the vyrc file will be missed as well as when
+# attempting to load a non existing file.
+if not args.verbose: sys.stderr = Debug()
+
 
 
 
