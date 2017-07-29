9b4434e4c01940395069e15c0430a27c3b87af9f
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-07-28 22:23:05 -0300

    Improving mode_shortcut plugin scheme.

--- vyapp/plugins/mode_shortcut.py
+++ vyapp/plugins/mode_shortcut.py
@@ -3,7 +3,7 @@ Overview
 ========
 
 Implement keycommands to pin a given mode and easily switch back to it.
-
+It is interesting to be used with modes like PDB etc.
 
 Key-Commands
 ============
@@ -14,8 +14,8 @@ Mode: GLOBAL
 Event: <Alt-n>
 Description: Pin the current mode.
 
-Mode: GLOBAL
-Event: <Alt-Escape>
+Mode: NORMAL
+Event: <Key-g>
 Description: Switch to the previous pinned mode.
 
 """
@@ -24,21 +24,21 @@ from vyapp.app import root
 
 class ModeShortcut(object):
     target_id  = 'NORMAL'
-    DEFAULT_ID = 'NORMAL'
 
     def __init__(self, area):
         self.area = area
-        area.install('mode-shortcut', ('-1', '<Alt-Escape>',  
+        area.install('mode-shortcut', ('NORMAL', '<Key-g>',  
         lambda event: self.area.chmode(self.target_id)),
         ('-1', '<Alt-n>',  lambda event: self.save_mode()))
                      
     def save_mode(self):
         self.target_id = self.area.id
-        self.area.chmode(self.DEFAULT_ID)
+        self.area.chmode('NORMAL')
         root.status.set_msg('Mode %s pinned!' % self.target_id)
         return 'break'
 
 install = ModeShortcut
+
 
 
 
