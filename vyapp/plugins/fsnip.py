aa8ddebe267f98bf8377e7a33d0d3b42edd49931
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-11-07 14:24:48 -0200

    Improving fsnip plugin.

--- vyapp/plugins/fsnip.py
+++ vyapp/plugins/fsnip.py
@@ -15,28 +15,36 @@ from vyapp.regutils import build_regex
 from vyapp.widgets import LinePicker
 from vyapp.areavi import AreaVi
 from vyapp.app import root
-from vyapp.ask import Ask
+from vyapp.ask import Get
 from re import findall
 
 class Fsnip:
     options = LinePicker()
     FOLDERS = []
     PATH   = 'ag'
+    fptr   = ''
     def  __init__(self, area):
         self.area = area
         area.install('fsnip', 
         (-1, '<Key-b>', lambda event: self.options.display()),
-        (-1, '<Key-B>', self.find))
+        (-1, '<Key-B>', lambda event: Get(events = {
+        '<Return>':self.find, 
+        '<Control-g>':self.set_file_pattern, 
+        '<Escape>':  lambda wid: True})))
 
-    def find(self, event):
+    def set_file_pattern(self, wid):
+        self.fptr = build_regex(wid.get())
+        root.status.set_msg('Set file pattern:%s' % self.fptr)
+        wid.delete(0, 'end')
+
+    def find(self, wid):
         """
         """
 
         root.status.set_msg('Set fsnip pattern!')
-        ask     = Ask()
-        pattern = build_regex(ask.data)
+        pattern = build_regex(wid.get())
         dirs    = ' '.join(self.FOLDERS)
-        child   = Popen([self.PATH, '--nocolor', '--nogroup', 
+        child   = Popen([self.PATH, '--nocolor', '--nogroup', '-G', self.fptr,
         '--vimgrep', '--noheading', pattern, dirs], stdout=PIPE, 
         stderr=STDOUT, encoding=self.area.charset)
 
@@ -47,6 +55,7 @@ class Fsnip:
             self.options(ranges)
         else:
             root.status.set_msg('No results:%s!' % pattern)
+        return True
 
 install = Fsnip
 
