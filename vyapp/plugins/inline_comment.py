99fae54285c9987573b052020cabe0eb6758a490
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-11-15 20:10:46 -0200

    Fixing misbehavior with inline_comment due to mimetype, using file extension to deduce a given language single comment character.

--- vyapp/plugins/inline_comment.py
+++ vyapp/plugins/inline_comment.py
@@ -19,17 +19,21 @@ Description: Remove inline comments from a selected block of text.
 
 """
 
-from mimetypes import guess_type
 from re import escape
+import os.path
 
 DEFAULT = '#'
 TABLE   = { 
-              'text/x-python'            :'#',
-              'text/x-java-source'       :'//',
-              'text/x-csrc'              :'//',
-              'text/x-sh'                :'#',
-              'application/x-javascript' :'//',
-              'text/x-c++src'            :'//'
+              '.py'   :'#',
+              '.java'  :'//',
+              '.c'    :'//',
+              '.sh'   :'#',
+              '.js'   :'//',
+              '.cpp'  :'//',
+              '.html' :'//',
+              '.go'   :'//',
+              '.rb' :'#',
+
           }
 
 def add_inline_comment(area):
@@ -37,7 +41,7 @@ def add_inline_comment(area):
     It adds inline comment to selected lines based on the file extesion.
     """
 
-    comment = TABLE.get(guess_type(area.filename)[0], DEFAULT)
+    comment = TABLE.get(os.path.splitext(area.filename)[1], DEFAULT)
     area.replace_ranges('sel', '^ +|^', 
     lambda data, index0, index1: '%s%s ' % (area.get(index0, index1), comment))
     area.clear_selection()
@@ -48,7 +52,7 @@ def rm_inline_comment(area):
     It removes the inline comments.
     """
 
-    comment = TABLE.get(guess_type(area.filename)[0], DEFAULT)
+    comment = TABLE.get(os.path.splitext(area.filename)[1], DEFAULT)
     area.replace_ranges('sel', '^ *%s ?' % comment, 
     lambda data, index0, index1: area.get(index0, index1).replace(
         '%s ' % comment, '').replace(comment, ''))
@@ -59,6 +63,7 @@ def install(area):
     area.install('inline-comment',
     ('ALPHA', '<Key-r>', lambda event: rm_inline_comment(event.widget)),
     ('ALPHA', '<Key-e>', lambda event: add_inline_comment(event.widget)))
+
 
 
 
