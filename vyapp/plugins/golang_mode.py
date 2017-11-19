56e0771409224a92ff9316c315669a7d8c2e706a
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-11-19 19:59:51 -0200

    Adding golang mode.

--- vyapp/plugins/golang_mode.py
+++ vyapp/plugins/golang_mode.py
@@ -1,1 +1,45 @@
+"""
+Overview
+========
+
+Extra mode for golang programming language.
+
+Key-Commands
+============
+
+Namespace: golang-mode
+
+Mode: NORMAL
+Event: <Key-exclam>
+Description: Switch to GOLANG mode.
+"""
+
+def golang_mode(area):
+    area.chmode('GOLANG')
+
+def install(area):
+    area.add_mode('PYTHON')
+    area.install('golang-mode', ('DELTA', '<Key-g>', 
+    lambda event: golang_mode(area)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
