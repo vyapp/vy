7bca2153e539078b24cc3df969a7e147fd71dc07
Iury de Oliveira Gomes Figueiredo <ioliveira@id.uff.br>
2018-02-04 11:46:09 +0000

    Improving tab_search plugin.

--- vyapp/plugins/tab_search.py
+++ vyapp/plugins/tab_search.py
@@ -12,17 +12,19 @@ class TabSearch(object):
         (-1, '<Alt-u>', lambda event: self.search_back()))
 
     def search_next(self):
-        get = Get(events={'<<Data>>': self.update_next, 
-        '<Alt-p>': self.next_tab, 
-        '<Alt-o>': self.prev_tab, 
+        get = Get(events={
+        '<<Data>>': lambda wid: root.note.switch_next(wid.get()), 
+        '<Alt-p>': lambda wid: root.note.switch_next(wid.get(), True), 
+        '<Alt-o>': lambda wid: root.note.switch_back(wid.get(), True), 
         '<Escape>': self.stop, 
         '<Return>': self.stop})
         return 'break'
 
     def search_back(self):
-        get = Get(events={'<<Data>>': self.update_back, 
-        '<Alt-p>': self.next_tab, 
-        '<Alt-o>': self.prev_tab, 
+        get = Get(events={
+        '<<Data>>': lambda wid: root.note.switch_back(wid.get()), 
+        '<Alt-p>': lambda wid: root.note.switch_next(wid.get(), True), 
+        '<Alt-o>': lambda wid: root.note.switch_back(wid.get(), True), 
         '<Escape>': self.stop, 
         '<Return>': self.stop})
         return 'break'
@@ -31,30 +33,8 @@ class TabSearch(object):
         root.note.set_area_focus()
         return True
 
-    def update_next(self, wid):
-        data = wid.get()
-        root.note.next(lambda text: data in text)
-
-    def next_tab(self, wid):
-        """
-
-        """
-        data = wid.get()
-        root.note.next(lambda text: data in text, True)
-
-    def update_back(self, wid):
-        data = wid.get()
-        root.note.back(lambda text: data in text)
-
-    def prev_tab(self, wid):
-        """
-
-        """
-        data = wid.get()
-        root.note.back(lambda text: data in text, True)
-
-    
 install = TabSearch
+
 
 
 
