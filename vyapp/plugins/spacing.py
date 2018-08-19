c8732c07eea5898ae4095fbd15129443cfe00e38
lury O. G. Figueiredo <ioliveira.id.uff.br>
2018-08-19 16:06:05 -0300

    Fixing misbehavior with spacing plugin when mmultiple file extensions are opened.

--- vyapp/plugins/spacing.py
+++ vyapp/plugins/spacing.py
@@ -20,44 +20,42 @@ from vyapp.plugins import ENV
 from vyapp.areavi import AreaVi
 
 class Tab(object):
-    DEFAULT_SIZE, DEFAULT_CHAR = 4, ' '
-    SCHEME, SIZE, CHAR         = {}, 4, ' '
+    default_size, default_char = 4, ' '
+    scheme = {}
 
     def __init__(self, area):
         self.area = area
-        area.install('spacing', 
-        (-1, '<<LoadData>>', self.set_tab_scheme),
-        (-1, '<<SaveData>>', self.set_tab_scheme),
-        (-1, '<<ClearData>>', self.set_tab_scheme),
-        ('INSERT', '<Tab>', lambda event: 
-        self.insert_tab(event.widget)))
+        area.install('spacing', ('INSERT', '<Tab>',  self.insert_tab))
     
-    def set_tab_scheme(self, event):
-        ph, ext = splitext(self.area.filename.lower())
-        opt     = self.SCHEME.get(
-        ext, (self.DEFAULT_SIZE, self.DEFAULT_CHAR))
+    def insert_tab(self, event):
+        ph, ext    = splitext(self.area.filename.lower())
+        size, char = self.scheme.get(ext, 
+        (self.default_size, self.default_char))
 
-        Tab.SIZE, Tab.CHAR = opt
-
-    def insert_tab(self, area):
-        area.edit_separator()
-        data = self.CHAR * self.SIZE
-
-        area.insert('insert', data)
+        self.area.edit_separator()
+        self.area.insert('insert', char * size)
         return 'break'
 
-def tabset(size, char=Tab.CHAR):
+    @classmethod
+    def set_scheme(cls, scheme={}):
+        cls.scheme.update(scheme)
+
+    @classmethod
+    def set_default(cls, default_size, default_char):
+        cls.default_size = default_size
+        cls.default_char = default_char
+
+def tabset(size, char):
     """
-    Change tab size/char globally based
+    Change tab default size/char globally based
     on the actual areavi filename extension.
     """
 
-    ph, ext = splitext(
-    AreaVi.ACTIVE.filename.lower())
-    Tab.SCHEME[ext]    = size, char
-    Tab.SIZE, Tab.CHAR = size, char
+    ph, ext = splitext(AreaVi.ACTIVE.filename.lower())
+    Tab.scheme[ext] = size, char 
 
 ENV['tabset'] = tabset
 install = Tab
+
 
 
