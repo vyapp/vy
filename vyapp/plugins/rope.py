d5d806b706d60e4f26ee8ecc3cd0ae1cc9b24bc6
Iury O. G. Figueiredo <ioliveira@id.uff.br>
2019-10-06 14:22:58 -0300

    Handling module renaming with rope plugin.

--- vyapp/plugins/rope.py
+++ vyapp/plugins/rope.py
@@ -3,7 +3,7 @@ Overview
 ========
 
 Integrates with python rope refactoring tools. It allows users
-to rename variables, methods, classes and also move python resources
+to rename variables, methods, classes, modules and also move python resources
 easily.
 
 Key-Commands
@@ -24,23 +24,50 @@ from vyapp.tools import get_project_root, error
 from vyapp.areavi import AreaVi
 from rope.refactor.rename import Rename
 from rope.base.libutils import path_to_resource
+from rope.base.change import MoveResource
 from vyapp.app import root
+from rope.base.evaluate import eval_location
 
 class PythonRefactor(object):
     def __init__(self, area):
-        self.area    = area
+        self.area  = area
+        self.files = None
         area.install('rope', ('PYTHON', '<Key-R>', error(self.pick_name)))
 
-    def update_instances(self, updates):
+    def update_instances(self, changes):
         """
         After changes it updates all AreaVi instances which 
         were changed.
         """
-        files = AreaVi.get_opened_files(root)
-        for ind in updates:
-            instance = files.get(ind.real_path)
-            if instance:
-                instance.load_data(ind.real_path)
+
+        # Avoid having to calculate it multiple times.
+        self.files = AreaVi.get_opened_files(root)
+        for ind in changes.changes:
+            if isinstance(ind, (MoveResource,)):
+                self.on_move_resource(ind)
+            else:
+                self.on_general_case(ind)
+
+    def on_general_case(self, change):
+        """
+        Should be called when self.files is updated.
+        """
+
+        for ind in change.get_changed_resources():
+           instance = self.files.get(ind.real_path)
+           if instance:
+               instance.load_data(ind.real_path)
+   
+    def on_move_resource(self, change):
+        """
+        Should be called when self.files is updated.
+        """
+        old, new = change.get_changed_resources()
+        instance = self.files.get(old.real_path)
+
+        # When the file is not updated then no need to load it.
+        if instance:
+            instance.load_data(new.real_path)
 
     def pick_name(self, event):
         ask = Ask()
@@ -57,12 +84,10 @@ class PythonRefactor(object):
         changes = renamer.get_changes(name)
         project.do(changes)
 
-        updates  = changes.get_changed_resources()
-        self.update_instances(updates)
+        self.update_instances(changes)
 
         print('\nRope - Renamed resource ..\n')
         print(changes.get_description())
-
         self.area.chmode('NORMAL')
         root.status.set_msg('Resources renamed!')
         project.close()
