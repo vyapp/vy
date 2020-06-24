69d7617be9c786986413152393bb89552d32a24b
Iury O. G. Figueiredo <last.src@gmail.com>
2020-06-24 19:19:45 +0000

    Improving inline_comment plugin code and key strokes.

--- vyapp/plugins/inline_comment.py
+++ vyapp/plugins/inline_comment.py
@@ -21,45 +21,49 @@ Description: Remove inline comments from a selected block of text.
 
 import os.path
 
-DEFAULT = '#'
-TABLE   = { 
-              '.py'   :'#',
-              '.java'  :'//',
-              '.c'    :'//',
-              '.sh'   :'#',
-              '.js'   :'//',
-              '.cpp'  :'//',
-              '.html' :'//',
-              '.go'   :'//',
-              '.rb' :'#',
+table   = { 
+    '.py'   :'#',
+    '.java'  :'//',
+    '.c'    :'//',
+    '.sh'   :'#',
+    '.js'   :'//',
+    '.cpp'  :'//',
+    '.html' :'//',
+    '.go'   :'//',
+    '.rb' :'#',
+}
 
-          }
+class Clipboard:
+    default = '#'
 
-def add_inline_comment(area):
-    """
-    It adds inline comment to selected lines based on the file extesion.
-    """
+    def __init__(self, area):
+        self.area = area
 
-    comment = TABLE.get(os.path.splitext(area.filename)[1], DEFAULT)
-    area.replace_ranges('sel', '^ *|^\t*', 
-    lambda data, index0, index1: '%s%s ' % (data, comment))
-    area.clear_selection()
-    area.chmode('NORMAL')
+        area.install('inline-comment',
+        ('EXTRA', '<Key-C>', self.remove_comment),
+        ('EXTRA', '<Key-c>', self.add_comment))
+    
+    def add_comment(self, event):
+        """
+        It adds inline comment to selected lines based on the file extesion.
+        """
+    
+        comment = table.get(os.path.splitext(self.area.filename)[1], self.default)
+        self.area.replace_ranges('sel', '^ *|^\t*', 
+        lambda data, index0, index1: '%s%s ' % (data, comment))
+        self.area.clear_selection()
+        self.area.chmode('NORMAL')
+    
+    def remove_comment(self, event):
+        """
+        It removes the inline comments.
+        """
+    
+        comment = table.get(os.path.splitext(self.area.filename)[1], self.default)
+        self.area.replace_ranges('sel', '^ *%s ?|^\t*%s ?' % (comment, comment), 
+        lambda data, index0, index1: data.replace(
+            '%s ' % comment, '').replace(comment, ''))
+        self.area.clear_selection()
+        self.area.chmode('NORMAL')
 
-def rm_inline_comment(area):
-    """
-    It removes the inline comments.
-    """
-
-    comment = TABLE.get(os.path.splitext(area.filename)[1], DEFAULT)
-    area.replace_ranges('sel', '^ *%s ?|^\t*%s ?' % (comment, comment), 
-    lambda data, index0, index1: data.replace(
-        '%s ' % comment, '').replace(comment, ''))
-    area.clear_selection()
-    area.chmode('NORMAL')
-
-def install(area):
-    area.install('inline-comment',
-    ('EXTRA', '<Key-r>', lambda event: rm_inline_comment(event.widget)),
-    ('EXTRA', '<Key-e>', lambda event: add_inline_comment(event.widget)))
-
+install = Clipboard
