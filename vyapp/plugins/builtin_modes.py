6ee099baf42b2d439a3396a8ee24cffe314b6cfe
Iury O. G. Figueiredo <last.src@gmail.com>
2021-07-15 01:24:12 -0300

    Removing ALPHA, BETA, ... modes.

--- vyapp/plugins/builtin_modes.py
+++ vyapp/plugins/builtin_modes.py
@@ -22,21 +22,6 @@ Mode: NORMAL
 Event: <Key-i>
 Description: Get the focused AreaVi instance in INSERT mode.
 
-Mode: NORMAL
-Event: <Key-4>
-Description: Get the AreaVi instance in BETA mode.
-
-Mode: NORMAL
-Event: <Key-6>
-Description: Get the AreaVi instance in DELTA mode.
-
-Mode: <NORMAL>
-Event: <Key-5>
-Description: Get the AreaVi instance that has focus in GAMMA mode.
-
-Mode: NORMAL
-Event: <Key-3>
-Description: Get the AreaVi instance that is focused in ALPHA mode.
 """
 
 class BuiltinModes:
@@ -47,44 +32,13 @@ class BuiltinModes:
         area.chmode('NORMAL')
    
         area.add_mode('INSERT', opt=True)
-        area.add_mode('DELTA')
-        area.add_mode('GAMMA')
-        area.add_mode('BETA')
-        area.add_mode('ALPHA')
         area.add_mode('EXTRA')
 
         area.install('builtin-modes', 
         (-1, '<Escape>', self.switch_normal),
         (-1, '<Alt-n>', self.switch_extra),
-        ('NORMAL', '<Key-i>', self.switch_insert),
-        ('NORMAL', '<Key-6>', self.switch_delta),
-        ('NORMAL', '<Key-5>', self.switch_gamma),
-        ('NORMAL', '<Key-4>', self.switch_beta),
-        ('NORMAL', '<Key-3>', self.switch_alpha))
+        ('NORMAL', '<Key-i>', self.switch_insert))
     
-    def switch_delta(self, event):
-        """
-        """
-        self.area.chmode('DELTA')
-
-    def switch_gamma(self, event):
-        """
-        """
-
-        self.area.chmode('GAMMA')
-
-    def switch_beta(self, event):
-        """
-        """
-
-        self.area.chmode('BETA')
-
-    def switch_alpha(self, event):
-        """
-        """
-
-        self.area.chmode('ALPHA')
-
     def switch_normal(self, event):
         """
         """
