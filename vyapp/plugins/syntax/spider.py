7cb01937bc6478e3e68a09cc4a11d9379887e0c1
Iury O. G. Figueiredo <last.src@gmail.com>
2021-11-26 15:41:41 -0300

    Improving API. Adding c_style classmethod to set style, adding better analysis to colorize visible regions. Adding better mechanism to detect lexer.

--- vyapp/plugins/syntax/spider.py
+++ vyapp/plugins/syntax/spider.py
@@ -2,36 +2,48 @@
     :)
 """
 from vyapp.plugins.syntax.tools import get_tokens_matrix
-from pygments.lexers import get_lexer_for_filename, guess_lexer, guess_lexer_for_filename
+from vyapp.plugins.syntax.styles.vy import VyStyle
+from pygments.util import ClassNotFound 
+from pygments.lexers import get_lexer_for_filename as get_lexer
+from pygments.lexers import guess_lexer
 from pygments.formatter import Formatter
 from pygments.filter import Filter
 from pygments.token import Token
 from itertools import groupby
 
-DEFAULT = (
-    'Token.Name.Tag',
-    'Token.Comment.Multiline',
-    'Token.Literal.String.Doc',
-    'Token.Literal.String.Heredoc',
-    'Token.Literal.String.Regex',
-    'Token.Literal.String.Symbol',
-    'Token.Literal.String',
-    'Token.Literal.String.Other',
-    'Token.Literal.String.Single',
-    'Token.Comment.Preproc',
-    'Token.Comment',
-    'Token.Comment.Single',
-    'Token.Comment.Special',
-    'Token.Literal.String.Double',
-    'Token.Literal.String.Backtick',
-    'Token.Literal.String.Char',
-    'Token.Literal.Number.Float',
-    'Token.Punctuation',
-    'Token.Operator',
-    'Token.Name',
-    'Token.Keyword',
-    'Token.Text',
+TK_ORDER = ('Token.Name.Tag', 'Token.Comment.Multiline',
+'Token.Literal.String.Doc','Token.Literal.String.Heredoc',
+'Token.Literal.String.Regex', 'Token.Literal.String.Symbol',
+'Token.Literal.String', 'Token.Literal.String.Other',
+'Token.Literal.String.Single', 'Token.Comment.Preproc',
+'Token.Comment', 'Token.Comment.Single',
+'Token.Comment.Special', 'Token.Literal.String.Double',
+'Token.Literal.String.Backtick', 'Token.Literal.String.Char',
+'Token.Literal.Number.Float', 'Token.Punctuation',
+'Token.Operator', 'Token.Name', 'Token.Keyword',
+'Token.Text',
 )
+
+def findlexer(filename, text, **opts):
+    """
+    The pygments guess_lexer_for_filename works only when there is
+    a lexer has a pattern in filename.
+    
+    When get_lexer_for_filename fails it uses the value in text
+    to guess the lexer using pygments guess_lexer.
+    """
+    try:
+        lexer = get_lexer(filename, **opts)
+    except ClassNotFound as e:
+        pass
+    else:
+        return lexer
+    try:
+        lexer = guess_lexer(text, **opts)
+    except ClassNotFound as e:
+        return None
+    else:
+        return lexer
 
 class JoinTType(Filter):
     def __init__(self, **options):
@@ -61,26 +73,34 @@ class TkTxtFormatter(Formatter):
         return ttype
 
 class Spider:
-    def  __init__(self, area, style, max=10):
+    style = VyStyle
+    def  __init__(self, area, max=10):
         self.area  = area
         self.max   = max
-        self.style = style
 
         area.install('syntax', 
         (-1, '<<LoadData>>', self.update_all),
         (-1, '<<SaveData>>', self.update_all),
         (-1, '<Escape>', self.update))
 
-        if style.background_color:
-            self.area.configure(background=style.background_color)
-        default_style = getattr(style, 'default_style')
+        if self.style.background_color:
+            self.area.configure(background=self.style.background_color)
+        default_style = getattr(self.style, 'default_style')
 
+        # Set foreground as pygments style.default_style value.
         if default_style:
             self.area.configure(foreground=default_style)
 
         for tokname, tokstyle in self.style:
             self.conf_tokstyle(str(tokname), tokstyle)
-        self.formatter = TkTxtFormatter(style=style)
+        self.formatter = TkTxtFormatter(style=self.style)
+        self.lexer = None
+
+    @classmethod
+    def c_style(cls, style):
+        """
+        """
+        cls.style = style
 
     def conf_tokstyle(self, tokname, tokstyle):
         foreground = tokstyle.get('color')
@@ -97,41 +117,43 @@ class Spider:
 
     def update_all(self, event):
         """
+        Colorize the whole text.
         """
         data = self.area.get('1.0', 'end')
-        lexer = guess_lexer_for_filename(self.area.filename, 
-        data[0:20], stripnl=False, stripall=False, tabsize=False)
+        self.lexer = findlexer(self.area.filename, 
+        data, stripnl=False, stripall=False, tabsize=False)
 
-        tokens = get_tokens_matrix(1, 0, data, lexer)
-        lexer.add_filter(JoinTType())
+        self.lexer.add_filter(JoinTType())
+
+        tokens = get_tokens_matrix(1, 0, data, self.lexer)
         self.formatter.format(tokens, self.area)
 
     def update(self, event):
         """
+        Colorize visible region.
         """
-
-        lexer = guess_lexer_for_filename(self.area.filename, 
-        self.area.get('insert -10l', 'insert +10l'), stripnl=False, 
-        stripall=False, tabsize=False)
-        lexer.add_filter(JoinTType())
 
         index0 = self.area.index('@0,0')
         index1 = self.area.index('%s -%sl' % (index0, self.max))
-        index2 = self.area.tag_prev_occur(DEFAULT, index0, index1, '1.0')
+        index2 = self.area.tag_prev_occur(TK_ORDER, index0, index1, '1.0')
 
         index3 = '@%s,%s' % (self.area.winfo_height(), self.area.winfo_width())
         index4 = self.area.index(index3)
         index5 = self.area.index('%s +%sl' % (index4, self.max))
-        index6 = self.area.tag_next_occur(DEFAULT, index4, index5, 'end')
-        index2 = self.area.index('%s -1l' % index2)
-        index6 = self.area.index('%s +1l' % index6)
+        index6 = self.area.tag_next_occur(TK_ORDER, index4, index5, 'end')
+
+        # Macumba attempt.
+        index7 = self.area.tag_prevrange('Token.Text', index2, '1.0')
+        index8 = self.area.tag_nextrange('Token.Text', index6, 'end')
+        index2 = index7[1] if index7 else index2
+        index6 = index8[0] if index8 else index6
 
         for ttype, tokstyle in self.style.styles.items():
             self.area.tag_remove(str(ttype), index2, index6)
         data = self.area.get(index2, index6)
 
         line, col = self.area.indexsplit(index2)
-        tokens = get_tokens_matrix(line, col, data, lexer)
+        tokens = get_tokens_matrix(line, col, data, self.lexer)
         self.formatter.format(tokens, self.area)
 
 install = Spider
